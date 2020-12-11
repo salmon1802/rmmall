70d1ad72ab55de55cb0547472ca11a4c09af70e9
salmon1802 <757635525@qq.com>
2020-12-11 17:22:36 +0800

    happyrmmall v1.0

--- imooc/src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ imooc/src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import sun.rmi.runtime.Log;
 
 import java.io.File;
 import java.io.IOException;
